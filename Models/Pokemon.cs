7e193034e2cde9075298897c43ac551066732047
Robert Basson <robertba@bbd.co.za>
2019-03-26 07:53:34 +0200

    Followed template

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1,138 +1,64 @@
-public class Pokemon{
- private int number,hp;
- private string name, description,status;
- private string[] type=new string[2],moveset=new string[4];
- private double weight,height;
- private  Pokemon preEvolve=null, Evolve=null;
+using System;
 
- private Image image = null;
-
- private bool finalForm=false,firstForm=false;
- 
- public Pokemon(string name){
-     this.name=name;
-     //get imfo from database
- }
-
-public Pokemon(int number){
-     this.number=number;
-     //get imfo from database
- }
- public void setFinalForm(bool finalForm){
-     this.finalForm=finalForm;
- }
- public bool getFinalForm(){
-     return this.finalForm;
- }
-
- public void setFirstForm(bool firstForm){
-     this.firstForm = firstForm;
- }
-
- public bool getFirstForm(){
-     return this.firstForm;
- }
- public void setNumber(int number){
-     this.number = number;
- }
- 
- public int getNumber(){
-     return this.number;
- }
-
- public void setHp(int hp){
-     this.hp=hp;
- }
-
- public int getHp(){
-     return this.hp;
- }
-
- public void setName(string name){
-     this.name=name;
- }
-
- public string getName(){
-     return this.name;
- }
-
- public void setDescription(string description){
-     this.description = description;
- }
-
- public string getDescription(){
-     return this.description;
- }
-
- public void setStatus(string status){
-     this.status = status;
- }
-
- public string getStatus(){
-     return this.status;
- }
-
- public void setTypes(string type1,string type2){
-     this.type[0]=type1;
-     if(!(type2==null || type2==""))
-        this.type[1]=type2;
-     else this.type[1] = null;   
- }
-
- public string[] getTypes(){
-     return this.getTypes;
- }
-
- public void setMoves(string move1,string move2,string move3,string move4){
-     this.moveset[0]= move1;
-     if(!(move2==null || move2==""))
-        this.moveset[1]=move2;
-     else this.moveset[1] = null;
-     if(!(move3==null || move3==""))
-        this.moveset[2]=move3;
-     else this.moveset[2] = null;
-     if(!(move4==null || move4==""))
-        this.moveset[3]=move4;
-     else this.moveset[3] = null;
- }
- public string[] getMoves(){
-     return this.moveset;
- }
- public void setWeight(double weight){
-     this.weight=weight;
- }
- public double getWeight(){
-     return this.weight;
- }
- public void setHeight(double height){
-     this.height=height;
- }
- public double getHeight(){
-     return this.height;
- }
- public void setImage(string url){
-     this.image=Image.FromFile(url);
- }
- public Image getImage(){
-     return this.image;
- }
-
- public string[] weaknesses(){return null;}
- public string[] strengths(){return null;}
-
- public void evolve(){
-     if(this.getFinalForm()||this.number+1>151){
-      this.Evolve =  null;
-      return;
+namespace Project.Models{
+    public abstract class Pokemon : IPokemon{
+      public enum Type
+        {
+            Normal, Fighting, Flying, Poison, Ground, Rock, Bug, Ghost, Steel,
+            Fire, Water, Grass, Electric, Psychic, Ice, Dragon, Dark, Fairy,
+            Unknown
+        }
+      public enum Rarity { Common, Uncommon, Rare }
+      public enum Move { /* TODO */ }
+      public uint number { get; private set; }
+      public string name { get; private set; }
+      public Type type1 { get; private set; }
+      public Type type2 { get; private set; }
+      public string description {get; private set;}
+      public double weight {get;private set;}
+      public double height {get;private set;}
+      public uint level { get; private set; }
+      public uint hp { get; private set; }
+      public IPokemon evolvesFrom{get;}
+      public IPokemon evolvesInto{get;}
+      public Rarity rarity{get;private set;}
+      public IPokemon(string name){
+        this.name=name;
+        //get imfo from database
      }
-     this.Evolve = new Pokemon(this.number+1);
- }
+      public IPokemon(int number){
+         this.number=number;
+         //get imfo from database
+     }
+     public Image image{get;}=null;
+     public bool firstForm{get;private set;}
+     public bool finalForm{get;private set;}
+     public Move move1{get;private set;}
+     public Move move2{get;private set;}
+     public Move move3{get;private set;}
+     public Move move4{get;private set;}
+     public Type[] weaknesses{get;}
+     public Type[] strengths{get;}
+     public void setImage(string url){
+         this.image = Image.FromFile(url);
+     }
 
- public void devolve(){
-     if(this.number-1<1|| this.getFirstForm()){
-         this.preEvolve=null;
+     public void evolve(){
+         if(this.finalForm){
+             this.evolvesInto = null;
+         }else this.evolvesInto = new IPokemon(this.number+1);
+     }
+     public void devolve(){
+         if(this.finalForm){
+             this.evolvesFrom = null;
+         } else this.evolvesFrom = new IPokemon(this.number-1);
+     }
+     public void setWeaknesses(){
          return;
-     }    
-     this.preEvolve = new Pokemon(this.number-1);
- }
+     }
+     public void setStrengths(){
+         return;
+     }
+
+    }
 }

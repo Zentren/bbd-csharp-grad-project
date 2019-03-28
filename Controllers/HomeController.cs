792acf131e5c56f375a843457da2416e0fa599ea
Vieira, Dario, Vodacom South Africa (External) <Dario.Vieira@vcontractor.co.za>
2019-03-28 15:04:33 +0200

    Revert "Added MVP Views"
    This reverts commit 0fb5e036316cb5f242eb2b60ce452f38ae9301c0.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,38 +8,22 @@ using Project.Models;
 
 namespace Project.Controllers
 {
-  public class HomeController : Controller
-  {
-    public IActionResult NameSearch()
+    public class HomeController : Controller
     {
-      return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult NumberSearch()
-    {
-      return View();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    public IActionResult HomePage()
-    {
-      return View();
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-
-    public IActionResult PokemonDisplay(String name = null, String number = null)
-    {
-      if (name != null){
-        ViewBag.Message = name;//Request["txtName"].ToString();
-      } else {
-        ViewBag.Message = number;
-      }
-      
-      return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-  }
 }

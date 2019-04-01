39a3c25c2ec5710f97cace1c890e33c7bd5159e2
Vieira, Dario, Vodacom South Africa (External) <Dario.Vieira@vcontractor.co.za>
2019-04-01 12:25:19 +0200

    Added pokemon object to view and controller

---
+++ Models/PokemonModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Speech.Synthesis;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.Threading.Tasks;
+
+namespace Project.Models{
+  public class PokemonModel {
+    public Pokemon getPokemonByName(String name){
+      return null;
+    }
+  }
+}
